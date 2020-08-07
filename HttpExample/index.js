fe96f1d99094d5bb1de94fca4cc4f537fc20e25e
Rishab Kumar <rishabkumar7@gmail.com>
2020-08-07 15:35:42 -0400

    Updated JSON response

--- HttpExample/index.js
+++ HttpExample/index.js
@@ -1,13 +1,114 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+module.exports = function(context, req) {
+    jsonData = {
+        "basics": {
+          "name": "Rishab Kumar",
+          "label": "CloudOps/DevOps",
+          "picture": "",
+          "email": "rishabkumar7@gmail.com",
+          "website": "http://rishabkumar.com",
+          "summary": "AWS and Linux entusiast with some experience in the Cloud.",
+          "location": {
+            "address": "Virginia Street",
+            "city": "Kingston",
+            "countryCode": "CA",
+            "region": "Ontario"
+          },
+          "profiles": [{
+            "network": "Twitter",
+            "username": "rishabk7",
+            "url": "http://twitter.com/rishabk7"
+          },
+          {
+            "network": "Linkedin",
+            "username": "Rishab Kumar",
+            "url": "https://www.linkedin.com/in/rishabkumar7/"
+          },
+          {
+            "network": "GitHub",
+            "username": "rishabkumar7",
+            "url": "https://github.com/rishabkumar7"
+          }]
+        },
+        "work": [{
+          "company": "ECi Software Solutions",
+          "position": "CloudOps Specialist",
+          "startDate": "2013-05-01",
+          "endDate": "Present",
+          "summary": "Description...",
+        }],
+        "education": [{
+          "institution": "St. Lawrence College",
+          "area": "Computer Networking",
+          "studyType": "Diploma",
+          "startDate": "2016-05-01",
+          "endDate": "2018-06-01",
+          "gpa": "3.5/4.0"
+        }],
+        "awards": [{
+          "title": "Dean's List Award",
+          "date": "2017-11-01",
+          "awarder": "St. Lawrence College",
+          "summary": "There is no spoon."
+        }],
+        "skills": [
+          {
+            "name": "AWS",
+            "level": "Intermediate",
+            "keywords": [
+              "Cloud",
+              "Lambda",
+              "S3",
+              "CloudFront"
+            ]
+          },
+          {
+            "name": "JavaScript",
+            "level": "Intermediate",
+            "keywords": [
+              "ECMAScript",
+              "ES6",
+              "Node.js",
+              "Web",
+              "Front End"
+            ]
+          },
+          {
+            "name": "React",
+            "level": "Intermediate",
+            "keywords": [
+              "SPA",
+              "Web",
+              "Front End"
+            ]
+          },
+          {
+            "name": "Vue",
+            "level": "Intermediate",
+            "keywords": [
+              "SPA",
+              "Web",
+              "Front End"
+            ]
+          },
+          {
+            "name": "Docker",
+            "level": "Intermediate",
+            "keywords": [
+              "Containers"
+            ]
+          }
+        ],
+        "interests": [{
+          "name": "Volleyball",
+          "keywords": [
+            "Beach",
+            "Court"
+          ]
+        }]
+      }
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+       body: JSON.stringify(jsonData, null, 2)
     };
-}
+    context.done();
+};
